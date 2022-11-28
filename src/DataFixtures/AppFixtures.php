97d257aaffc60cebef54fe86c9b24800c4a6d8d6
Egidio <egidio.speranza@storykube.com>
2022-11-28 13:45:23 +0100

    Study Fake Data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,36 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use DateTime;
+use App\Entity\SocialPost;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $socialPost1 = New SocialPost();
+        $socialPost1 -> setTitle('Welcome!');
+        $socialPost1 -> setText('Welcome!');
+        $socialPost1 -> setCreated(new DateTime());
+
+        $manager -> persist($socialPost1);
+
+        $socialPost2 = New SocialPost();
+        $socialPost2 -> setTitle('Welcome!');
+        $socialPost2 -> setText('Welcome!');
+        $socialPost2 -> setCreated(new DateTime());
+
+        $manager -> persist($socialPost2);
+
+        $socialPost3 = New SocialPost();
+        $socialPost3 -> setTitle('Welcome!');
+        $socialPost3 -> setText('Welcome!');
+        $socialPost3 -> setCreated(new DateTime());
+
+        $manager -> persist($socialPost3);
+
 
         $manager->flush();
     }
