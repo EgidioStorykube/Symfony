e3a86fda74004c3c03a955725d45510f0b8e04e2
Egidio <egidio.speranza@storykube.com>
2022-11-28 22:47:18 +0100

    Study convert

--- src/Controller/SocialPostController.php
+++ src/Controller/SocialPostController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\SocialPost;
+use App\Repository\SocialPostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SocialPostController extends AbstractController
 {
-    #[Route('/social/post', name: 'app_social_post')]
-    public function index(): Response
+    #[Route('/social/posts', name: 'app_social_posts')]
+    public function index(SocialPostRepository $socialPosts): Response
     {
+
         return $this->render('social_post/index.html.twig', [
-            'controller_name' => 'SocialPostController',
+            'socialPosts' => $socialPosts->findAll(),
         ]);
     }
+
+    #[Route('/social/post/{socialPost}', name: 'app_social_post_show')]
+    public function showOne(SocialPost $socialPost): Response
+    {
+
+        return $this->render('social_post/show.html.twig', [
+            'socialPost' => $socialPost,
+        ]);
+
+    }
+
 }
