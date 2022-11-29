063b107b4e0d83ff8e6338e57395292e7e5edabb
Egidio <egidio.speranza@storykube.com>
2022-11-29 16:19:18 +0100

    Study Forms class

--- src/Controller/SocialPostController.php
+++ src/Controller/SocialPostController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\SocialPost;
+use App\Form\SocialPostType;
 use App\Repository\SocialPostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SocialPostController extends AbstractController
@@ -25,6 +29,55 @@ class SocialPostController extends AbstractController
 
         return $this->render('social_post/show.html.twig', [
             'socialPost' => $socialPost,
+        ]);
+
+    }
+
+    #[Route('/social/post/add', name: 'app_social_post_add', priority: 2)]
+    public function add(Request $request, SocialPostRepository $socialPosts):Response
+    {
+        $form = $this -> createForm(SocialPostType::class, new SocialPost());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $socialPost = $form->getData();
+            $socialPost->setCreated(new DateTime());
+            $socialPosts->save($socialPost, true);
+
+            //add a flash
+            $this->addFlash('success', 'Your post have been created');
+
+            //redirect
+            return $this->redirectToRoute('app_social_posts');
+        }
+
+        return $this->renderForm('social_post/add.html.twig', [
+            'form' => $form,
+        ]);
+
+    }
+
+    #[Route('/social/post/{socialPost}/edit', name: 'app_social_post_edit')]
+    public function edit(SocialPost $socialPost, Request $request, SocialPostRepository $socialPosts):Response
+    {
+        $form = $this -> createForm(SocialPostType::class, $socialPost);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $socialPost = $form->getData();
+            $socialPosts->save($socialPost, true);
+
+            //add a flash
+            $this->addFlash('success', 'Your post have been Updated');
+
+            //redirect
+            return $this->redirectToRoute('app_social_posts');
+        }
+
+        return $this->renderForm('social_post/edit.html.twig', [
+            'form' => $form,
         ]);
 
     }
